755b4b473453ee5ac191e2bbf980631e4f587084
Anshuman Jadiya <anshuman.jadiya03@gmail.com>
2025-09-17 18:02:36 +0530

    Implement QAEngine to handle question-answer retrieval
    This class loads a list of QA pairs from a JSON file and provides methods to retrieve answers based on questions.

---
+++ src/main/java/com/javahelper/qa/QAEngine.java
@@ -0,0 +1,39 @@
+package com.javahelper.qa;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class QAEngine {
+
+    private final List<QA> knowledge;
+
+    public QAEngine(String knowledgeFile) throws Exception {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<QA>>(){}.getType();
+        knowledge = gson.fromJson(new FileReader(knowledgeFile), listType);
+    }
+
+    public String getAnswer(String question) {
+        question = question.toLowerCase();
+        for (QA qa : knowledge) {
+            if (qa.question.toLowerCase().contains(question) || question.contains(qa.question.toLowerCase())) {
+                return qa.answer;
+            }
+        }
+        return "Sorry, I don't know the answer. Try another question!";
+    }
+
+    public static class QA {
+        String category;
+        String question;
+        String answer;
+    }
+
+    public List<QA> getKnowledge() {
+        return knowledge;
+    }
+}
